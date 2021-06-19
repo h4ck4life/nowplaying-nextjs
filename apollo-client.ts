133a16ba8228f68b9f8cd29816cf80a404ee015b
Alif <alifaziz@gmail.com>
2021-06-19 23:44:35 +0800

    remove cache

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,10 +1,22 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
 
 const client = new ApolloClient({
   uri: "https://tmdb.apps.quintero.io",
   cache: new InMemoryCache({
-    resultCaching: false
+    resultCaching: false,
   }),
+  defaultOptions: defaultOptions,
 });
 
 export default client;
